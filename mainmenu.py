a45565b1bdcbe338894be37e50434a762f55a00f
Alain Basty <alain.basty@free.fr>
2011-10-22 23:12:40 +0200

    Added simple keys + mouse selection in main menu. Also changed colors.

--- mainmenu.py
+++ mainmenu.py
@@ -4,14 +4,14 @@ import pygame
 import game
 
 # Global Declarations
-menu = [ "Play", "Setup", "About", " ", "Quit" ]
+menu = [ "Play", "Setup", "About", "Quit" ]
 zones = [ ]
 renderedText = [ ]
 selected = -1
+fontSize = 100
 
 # Initialization
 def init():
-    fontSize = 100
     font = pygame.font.Font(None, fontSize)
     maxWidth = 0
     for item in menu:
@@ -31,22 +31,47 @@ def enter():
 
 # Event callback
 def event(event):
+    global selected
+    
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
             return "Quit"
-        elif event.key == pygame.K_RETURN or event.key == pygame.K_p:
-            return "Play"
+        elif event.key == pygame.K_RETURN:
+            if selected == -1:
+                return "Play"
+            else:
+                return menu[selected]
+        elif event.key == pygame.K_DOWN:
+            selected = (selected + 1) % len(menu)
+        elif event.key == pygame.K_UP:
+            selected = (selected + len(menu) - 1) % len(menu)
+        return
 
     if event.type == pygame.MOUSEBUTTONDOWN:
         for i in range(len(zones)):
             if zones[i].collidepoint(event.pos):
                 return menu[i]
+        return
+
+    if event.type == pygame.MOUSEMOTION:
+        selected = -1
+        for i in range(len(zones)):
+            if zones[i].collidepoint(event.pos):
+                selected = i
+        return
 
 # Draw callback
 def draw():
-    game.screen.fill([255, 255, 255])
+    game.screen.fill(0xb4b4d9)
+    if selected != -1:
+        r = zones[selected].copy()
+        r.left = 20
+        r.width = game.size[0] - 2 * r.left
+        r.top -= r.height / 4
+        pygame.draw.rect(game.screen, 0xffffff, r, 4)
+        r.inflate(-4, -4)
+        pygame.draw.rect(game.screen, 0x5b5ba0, r)
     for i in range(len(renderedText)):
         game.screen.blit(renderedText[i], zones[i].topleft)
-
 
 init()
