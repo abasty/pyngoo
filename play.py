5e52cfc1455c2b0cf9c65b321db96ed712365a9b
Alain Basty <alain.basty@free.fr>
2011-10-22 21:02:52 +0200

    Added Labyrinth computation and basic draw function

--- play.py
+++ play.py
@@ -1,21 +1,33 @@
 # This module defines the play state
+
+import random
 import pygame
 import game
 
 title = "Play"
 font = pygame.font.Font(None, 50)
-
-blocks = pygame.sprite.Group()
-
-class block(pygame.sprite.Sprite):
-    pass 
+tableau = []
+spriteSize = 30
 
 # Initialization
 def init():
     pass
 
 def enter():
-    pass
+    # Labyrinth
+    global tableau
+    tableau = []
+    for l in range(10):
+        ligne0 = []
+        ligne1 = []
+        for c in range(12):
+            r = random.choice(["xx..", "xx..", "x.x.", "x.x.", "x..."])
+            ligne0.append(r[0])
+            ligne0.append(r[1])
+            ligne1.append(r[2])
+            ligne1.append(r[3])
+        tableau.append(ligne0)
+        tableau.append(ligne1)
 
 # Event callback
 def event(event):
@@ -28,5 +40,12 @@ def event(event):
 def draw():
     game.screen.fill([255, 0, 255])
     game.screen.blit(font.render(title, True, [0, 0, 0]), [0, 0])
-    
+
+    for l in range(len(tableau)):
+        ligne = tableau[l]
+        for c in range(len(ligne)):
+            item = ligne[c]
+            if item == "x":
+                pygame.draw.rect(game.screen, 0, pygame.Rect(c * spriteSize + 20 + 90, l * spriteSize, spriteSize, spriteSize))
+
 init()
