8aac23b194d8b56dcb69c4d51d99804dde7d7136
Alain Basty <alain.basty@free.fr>
2011-11-20 19:06:27 +0100

    Move to sprites step 1.

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
+import os
 import pygame
+from pygame.locals import RLEACCEL
 
 # Define the colors we will use in RGB format
 black = [  0,  0,  0]
@@ -13,11 +15,29 @@ pi = 3.141592653
 # Set the height and width of the game.screen
 size = [ 800, 600 ]
 
+# Decal on play screen
+xorigin = 20
+yorigin = 60
+
 #screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("PyGame Example - Alain Basty")
 
 MOTIONTICK = pygame.USEREVENT
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('media', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
 
 class Object:
     pass
--- play.py
+++ play.py
@@ -2,16 +2,17 @@
 
 import random
 import pygame
-import game
+from game import Object, MOTIONTICK, screen
+from block import Border, Block
 
 #font = pygame.font.Font(None, 50)
 
 back = pygame.image.load("media/playbackground.png").convert()
-glacon = pygame.image.load("media/glacon.png").convert()
-glacon.set_colorkey(glacon.get_at((0,0)), pygame.RLEACCEL)
-bord = pygame.image.load("media/igloo.jpg").convert()
-neige = pygame.image.load("media/neige.jpg").convert()
-tux = pygame.image.load("media/tux.png").convert_alpha()
+#glacon = pygame.image.load("media/glacon.png").convert()
+#glacon.set_colorkey(glacon.get_at((0,0)), pygame.RLEACCEL)
+#bord = pygame.image.load("media/igloo.jpg").convert()
+#neige = pygame.image.load("media/neige.jpg").convert()
+#tux = pygame.image.load("media/tux.png").convert_alpha()
 
 
 # Move should be computed on time (see clock) because tick is not stable
@@ -23,29 +24,19 @@ ticknumber = 0
 
 lmax = 13
 cmax = 19
-lorigin = 60
-corigin = 20
 
-player = game.Object()
+player = Object()
 
 # Initialization
 def init():
     pass
 
 def enter():
-    # Player
-    global player
-    player.lig = int(lmax / 2)
-    player.col = int(cmax / 2)
-    player.dlig = 0
-    player.dcol = 0
-    player.x = player.col * blocksize + corigin
-    player.y = player.lig * blocksize + lorigin
-    player.key = -1
-    player.picture = tux
+    
+    global labyrinth
+    labyrinth = pygame.sprite.RenderPlain()
 
     # Labyrinth
-    global tableau
     tableau = []
     ligne0 = [ "b" ] * cmax
     tableau.append(ligne0)
@@ -66,13 +57,21 @@ def enter():
         tableau.append(ligne1)
     ligne0 = [ "b" ] * cmax
     tableau.append(ligne0)
-    tableau[player.lig][player.col] = "."
 
-    pygame.time.set_timer(game.MOTIONTICK, motionticktime)
+    for l in range(lmax):
+        for c in range(cmax):
+            p = tableau[l][c]
+            if p is "b":
+                labyrinth.add(Border(l, c))
+            if p is "x":
+                labyrinth.add(Block(l, c))
+    
+    pygame.time.set_timer(MOTIONTICK, motionticktime)
 
 def leave():
-    pygame.time.set_timer(game.MOTIONTICK, 0)
-    pass
+    global labyrinth
+    labyrinth.empty()
+    pygame.time.set_timer(MOTIONTICK, 0)
 
 def define_move(key):
     player.key = key
@@ -83,41 +82,10 @@ def undefine_move_if_equal(key):
 #        player.key = -1
 
 def begin_move():
-    # end move
-    player.col += player.dcol
-    player.lig += player.dlig
-    player.dcol = 0
-    player.dlig = 0
-    player.dx = 0
-    player.dy = 0
-
-    if player.key == pygame.K_UP:
-        dcol = 0
-        dlig = -1
-    elif player.key == pygame.K_DOWN:
-        dcol = 0
-        dlig = 1
-    elif player.key == pygame.K_LEFT:
-        dcol = -1
-        dlig = 0
-    elif player.key == pygame.K_RIGHT:
-        dcol = 1
-        dlig = 0
-    else:
-        dcol = 0
-        dlig = 0
-
-    collision = tableau[player.lig + dlig][player.col + dcol]
-    
-    if collision == ".":
-        player.dcol = dcol
-        player.dlig = dlig
-        player.dx = dcol * movesize
-        player.dy = dlig * movesize
+    pass
 
 def move():
-    player.x += player.dx
-    player.y += player.dy
+    pass
 
 def motiontick():
     global ticknumber
@@ -141,28 +109,16 @@ def event(event):
 #        undefine_move_if_equal(event.key)
  #       return
 
-    if event.type == game.MOTIONTICK:
+    if event.type == MOTIONTICK:
         motiontick()
 
 # Draw callback
 def draw():
-    game.screen.blit(back, [0, 0])
+    screen.blit(back, [0, 0])
 
     # draw board
-    y = lorigin
-    for l in range(len(tableau)):
-        ligne = tableau[l]
-        x = corigin
-        for c in range(len(ligne)):
-            item = ligne[c]
-            if item == "x":
-                game.screen.blit(glacon, [ x, y ])
-            elif item == "b":
-                game.screen.blit(bord, [ x, y ])
-            x += blocksize
-        y += blocksize
-
+    labyrinth.draw(screen)
     # draw player
-    game.screen.blit(player.picture, [ player.x, player.y ])
+#    game.screen.blit(player.picture, [ player.x, player.y ])
 
 init()
