54894d285e3c09d2371e143d9554252944c6cb7c
Alain Basty <alain.basty@free.fr>
2011-12-15 22:29:59 +0100

    Added a FIXME comment to implement an image cache

--- game.py
+++ game.py
@@ -27,6 +27,9 @@ MOTIONTICK = pygame.USEREVENT
 COLORKEY_AUTO = -1
 ALPHA = -2
 
+# FIXME: Implement image cache = create an associative array between image name and image itself
+# FIXME: Do not load the image if the image is already available in the cache
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('media', name)
     try:
